d24b831f7812aaf192d12cae401c1f9a5ac2d557
John Dilig <balbonits@yahoo.com>
2015-10-22 12:15:42 -0700

    adding ngCordova module,adding new dialog design from ngCordova

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','firebase','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -18,20 +18,29 @@ angular.module('starter', ['ionic'])
   });
 })
 
-.controller('ListCtrl', ['$scope','$ionicListDelegate', function($scope,$ionicListDelegate){
-  $scope.items = [];
+.factory('ItemsFactory',['$firebaseArray',function($firebaseArray){
+  var todoRef = new Firebase('https://blistering-heat-1902.firebaseio.com/todos');
+
+  return $firebaseArray(todoRef);
+}])
+
+.controller('ListCtrl', ['$scope','$ionicListDelegate','$cordovaDialogs','ItemsFactory', function($scope,$ionicListDelegate,$cordovaDialogs,ItemsFactory){
+  $scope.items = ItemsFactory;
 
   // add new item button
   $scope.addNew = function(){
-    var name = prompt('What dp you need to do>');
-    if(name) $scope.items.push({'name':name});
+    $cordovaDialogs.prompt('What do you need to do?','New Task',['Cancel','Add'],'')
+    .then(function(result){
+      if(result.buttonIndex == 2) {
+        $scope.items.$add({'name':result.input1});
+      }
+    });
   }
 
   //remove selected item button
-  $scope.removeItem = function(item){
-    $scope.item = item;
-    $scope.item['status'] = 'done';
+  $scope.markItem = function(item){
+    var todoRef = new Firebase('https://blistering-heat-1902.firebaseio.com/todos/' + item.$id);
+    todoRef.child('status').set('done');
     $ionicListDelegate.closeOptionButtons();
-    $scope.item = null;
   }
 }])
